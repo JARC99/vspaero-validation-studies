1ed4133d3b36b351c834811a84bf7844e3c6c73e
José Alfredo Rosas Córdova <jarosascordova@gmail.com>
2022-07-07 21:43:44 +0200

    Case 10

--- validation_cs_10/validation_cs_10-lift_curve.py
+++ validation_cs_10/validation_cs_10-lift_curve.py
@@ -39,7 +39,8 @@ twist_array = np.array([0.0, -2.0])
 tap_rat = tip_chord/root_chord
 chord_mgc = 2/3 * root_chord * (1 + tap_rat + tap_rat**2)/(1 + tap_rat)
 yloc_mgc = midspan/3 * (1 + 2*tap_rat)/(1 + tap_rat)
-xloc_mgc = yloc_mgc*np.tan(sweep*np.pi/180) + chord_mgc/4
+xloc_mgc = root_chord/4
+zloc_mgc = 0.025
 
 xseccurve_type = vsp.XS_SIX_SERIES
 tc_rat = 0.10
@@ -55,9 +56,9 @@ le_clstr = 0.25
 te_clstr = 0.25
 
 # Flow conditions
-alpha_i = -3
+alpha_i = -4
 alpha_f = 13
-alpha_npts = 17
+alpha_npts = 18
 alpha_array = np.linspace(alpha_i, alpha_f, alpha_npts)
 
 mach = 0.17
@@ -65,46 +66,84 @@ mach = 0.17
 # Known results
 prcnt_error = 0.05
 
-x_wsh0 = [-2.960000000000001, -1.8400000000000056, -0.8800000000000026,
-          0.1599999999999966, 1.1999999999999957, 2.239999999999995,
-          3.4399999999999906, 4.47999999999999, 5.519999999999989,
-          6.63999999999999, 7.679999999999989, 8.639999999999986,
-          9.759999999999982, 10.879999999999981, 11.439999999999982,
-          11.999999999999982, 12.479999999999976, 12.95999999999998,
-          13.599999999999977, 13.999999999999975]
-y_wsh0 = [-0.14799999999999985, -0.05599999999999999, 0.03599999999999989,
-          0.12000000000000008, 0.21600000000000022, 0.31600000000000006,
-          0.39933331298828134, 0.4833333129882811, 0.5833333129882812,
-          0.6673333129882814, 0.7673333129882813, 0.8593333129882812,
-          0.9593333129882812, 1.031333312988281, 1.079333312988281,
-          1.119333312988281, 1.163333312988281, 1.203333312988281,
-          1.239333312988281, 1.0873333129882812]
-expCL_wsh0 = np.array([x_wsh0, y_wsh0]).T
+xCL_wsh0 = [-2.960000000000001, -1.8400000000000056, -0.8800000000000026,
+            0.1599999999999966, 1.1999999999999957, 2.239999999999995,
+            3.4399999999999906, 4.47999999999999, 5.519999999999989,
+            6.63999999999999, 7.679999999999989, 8.639999999999986,
+            9.759999999999982, 10.879999999999981, 11.439999999999982,
+            11.999999999999982, 12.479999999999976, 12.95999999999998,
+            13.599999999999977, 13.999999999999975]
+yCL_wsh0 = [-0.14799999999999985, -0.05599999999999999, 0.03599999999999989,
+            0.12000000000000008, 0.21600000000000022, 0.31600000000000006,
+            0.39933331298828134, 0.4833333129882811, 0.5833333129882812,
+            0.6673333129882814, 0.7673333129882813, 0.8593333129882812,
+            0.9593333129882812, 1.031333312988281, 1.079333312988281,
+            1.119333312988281, 1.163333312988281, 1.203333312988281,
+            1.239333312988281, 1.0873333129882812]
+expCL_wsh0 = np.array([xCL_wsh0, yCL_wsh0]).T
 
-x_wsh2 = [-2.988142292490118, -1.9920948616600789, -0.9486166007905138,
-          0.23715415019762845, 1.233201581027668, 2.276679841897233,
-          3.4150197628458496, 4.411067193675889, 5.50197628458498,
-          6.4980237154150196, 6.545454545454545, 7.541501976284584,
-          8.632411067193676, 9.723320158102766, 10.861660079051383,
-          11.288537549407113, 12.42687747035573, 12.901185770750986,
-          13.42292490118577, 14.039525691699604, 14.466403162055334]
-y_wsh2 = [-0.174390243902439, -0.08414634146341464, 0.013414634146341461,
-          0.10365853658536583, 0.19878048780487803, 0.28414634146341466,
-          0.38170731707317074, 0.4670731707317073, 0.5658536585365854,
-          0.6487804878048781, 0.6707317073170732, 0.7463414634146341,
-          0.8439024390243902, 0.9317073170731708, 1.0219512195121951,
-          1.073170731707317, 1.1536585365853658, 1.1926829268292682,
-          1.226829268292683, 1.2317073170731707, 1.1317073170731706]
-expCL_wsh2 = np.array([x_wsh2, y_wsh2]).T
+xCL_wsh2 = [-2.988142292490118, -1.9920948616600789, -0.9486166007905138,
+            0.23715415019762845, 1.233201581027668, 2.276679841897233,
+            3.4150197628458496, 4.411067193675889, 5.50197628458498,
+            6.4980237154150196, 6.545454545454545, 7.541501976284584,
+            8.632411067193676, 9.723320158102766, 10.861660079051383,
+            11.288537549407113, 12.42687747035573, 12.901185770750986,
+            13.42292490118577, 14.039525691699604, 14.466403162055334]
+yCL_wsh2 = [-0.174390243902439, -0.08414634146341464, 0.013414634146341461,
+            0.10365853658536583, 0.19878048780487803, 0.28414634146341466,
+            0.38170731707317074, 0.4670731707317073, 0.5658536585365854,
+            0.6487804878048781, 0.6707317073170732, 0.7463414634146341,
+            0.8439024390243902, 0.9317073170731708, 1.0219512195121951,
+            1.073170731707317, 1.1536585365853658, 1.1926829268292682,
+            1.226829268292683, 1.2317073170731707, 1.1317073170731706]
+expCL_wsh2 = np.array([xCL_wsh2, yCL_wsh2]).T
 
 expCL_array_list = [expCL_wsh0, expCL_wsh2]
 
 
+xCM_wsh0 = [-0.036000000000000004, -0.036000000000000004,
+            -0.037333333333333336, -0.037333333333333336, -0.03866666666666667,
+            -0.03866666666666667, -0.037333333333333336, -0.036000000000000004,
+            -0.036000000000000004, -0.03333333333333334, -0.036000000000000004,
+            -0.03333333333333334, -0.03466666666666667, -0.032, -0.032, -0.032,
+            -0.029333333333333336, -0.028000000000000004,
+            -0.029333333333333336, -0.029333333333333336,
+            -0.026666666666666672, -0.028000000000000004, -0.05066666666666667]
+yCM_wsh0 = [-0.14, -0.14, -0.04666666666666666, -0.04666666666666666,
+            0.04133333333333336, 0.04133333333333336, 0.12400000000000005,
+            0.22000000000000003, 0.316, 0.3960000000000001,
+            0.48133333333333345, 0.5840000000000001, 0.6693333333333333,
+            0.7733333333333334, 0.8533333333333335, 0.9466666666666668,
+            1.0266666666666668, 1.0693333333333335, 1.1093333333333335,
+            1.1493333333333335, 1.194666666666667, 1.2346666666666668,
+            1.0826666666666669]
+expCM_wsh0 = np.array([xCM_wsh0, yCM_wsh0]).T
+
+
+xCM_wsh2 = [-0.03684210526315789, -0.035526315789473684, -0.03684210526315789,
+            -0.03684210526315789, -0.03684210526315789, -0.03684210526315789,
+            -0.03684210526315789, -0.035526315789473684, -0.03684210526315789,
+            -0.031578947368421054, -0.03684210526315789, -0.035526315789473684,
+            -0.034210526315789476, -0.034210526315789476, -0.03289473684210526,
+            -0.035526315789473684, -0.03289473684210526, -0.030263157894736843,
+            -0.030263157894736843, -0.02763157894736842, -0.05526315789473684]
+yCM_wsh2 = [-0.17866666666666667, -0.08, 0.008000000000000007,
+            0.09866666666666668, 0.19200000000000006, 0.28,
+            0.37760001627604173, 0.4576000162760417, 0.5536000162760417,
+            0.6416000162760418, 0.6576000162760418, 0.7322666829427085,
+            0.8282666829427086, 0.9216000162760418, 1.0069333496093753,
+            1.0570666910807294, 1.0970666910807294, 1.1370666910807294,
+            1.1744000244140627, 1.2090666910807293, 1.115733357747396]
+expCM_wsh2 = np.array([xCM_wsh2, yCM_wsh2]).T
+
+expCM_array_list = [expCM_wsh0, expCM_wsh2]
+
+
 # %%Define funxtion to set analysis parameters
 
-
 def setVSPAEROsweepinput(alpha_i, alpha_f, alpha_npts, mach=0.0, x_cg=0.0,
-                         y_cg=0.0, z_cg=0.0):
+                         y_cg=0.0, z_cg=0.0,
+                         chord_mgc=(root_chord+tip_chord)/2):
     with open(VSPAERODIR) as file_object:
         vspscriptlines = file_object.readlines()
     vspscriptlines[10] = '	  string fname = "{0}";\n'.format(FNAME)
@@ -113,9 +152,11 @@ def setVSPAEROsweepinput(alpha_i, alpha_f, alpha_npts, mach=0.0, x_cg=0.0,
     vspscriptlines[57] = "    double alpha_npts = {0};\n".format(alpha_npts)
     vspscriptlines[62] = "    double mach_i = {0:.2f};\n".format(mach)
 
-    vspscriptlines[68] = "    double x_cg = {0:.2f};\n".format(x_cg)
-    vspscriptlines[73] = "    double y_cg = {0:.2f};\n".format(y_cg)
-    vspscriptlines[78] = "    double z_cg = {0:.2f};\n".format(z_cg)
+    vspscriptlines[68] = "    double x_cg = {0:.3f};\n".format(x_cg)
+    vspscriptlines[73] = "    double y_cg = {0:.3f};\n".format(y_cg)
+    vspscriptlines[78] = "    double z_cg = {0:.3f};\n".format(z_cg)
+
+    vspscriptlines[84] = "    double chord_mgc = {0:.3f};\n".format(chord_mgc)
 
     with open(VSPAERODIR, "w") as file_object:
         file_object.writelines(vspscriptlines)
@@ -123,7 +164,8 @@ def setVSPAEROsweepinput(alpha_i, alpha_f, alpha_npts, mach=0.0, x_cg=0.0,
 
 # %% Create OpenVSP file
 
-fig, ax1 = plt.subplots(1, sharey=True, dpi=DPI)
+fig1, ax1 = plt.subplots(1, sharey=True, dpi=DPI)
+fig2, ax2 = plt.subplots(1, sharex=True, dpi=DPI)
 for i, twist in enumerate(twist_array):
     vsp.ClearVSPModel()
 
@@ -165,7 +207,8 @@ for i, twist in enumerate(twist_array):
     vsp.Update()
 
     setVSPAEROsweepinput(alpha_i, alpha_f, alpha_npts,
-                         mach=mach, x_cg=xloc_mgc)
+                         mach=mach, x_cg=xloc_mgc, z_cg=zloc_mgc,
+                         chord_mgc=chord_mgc)
 
     vsp.ClearVSPModel()
     vsp.ReadVSPFile(FDIR)
@@ -189,31 +232,30 @@ for i, twist in enumerate(twist_array):
              label="Experimental, " +
              r"$\mathdefault{\phi_{G}}=$" + "{0}°".format(int(twist)))
     # ax1.fill_between(expCL_array_list[i][:, 0],
-                     # expCL_array_list[i][:, 1]*(1 + prcnt_error),
-                     # expCL_array_list[i][:, 1]*(1 - prcnt_error), alpha=0.25,
-                     # color=PALETTE[i])
-    # ax1.set_ylim(bottom=-0.4)
+    # expCL_array_list[i][:, 1]*(1 + prcnt_error),
+    # expCL_array_list[i][:, 1]*(1 - prcnt_error), alpha=0.25,
+    # color=PALETTE[i])
     ax1.set_xlabel(r"$\mathdefault{\alpha}$, °")
     ax1.set_ylabel(r"$\mathdefault{C_{L}}$")
     ax1.legend()
 
-    fig.savefig(os.path.join(GRAPHICS_DIR, "lift_curves.pdf"), format="pdf",
-                bbox_inches="tight")
+    fig1.savefig(os.path.join(GRAPHICS_DIR, "lift_curves.pdf"), format="pdf",
+                 bbox_inches="tight")
 
-# %%
+    ax2.plot(CL_array, CM_array, label="VSPAERO, " + r"$\mathdefault{\phi_{G}}=$" +
+             "{0}°".format(int(twist)))
+    ax2.plot(expCM_array_list[i][:, 1], expCM_array_list[i][:, 0],
+             linestyle="None", color=PALETTE[i], marker=MARKERS[1],
+             label="Experimental, " +
+             r"$\mathdefault{\phi_{G}}=$" + "{0}°".format(int(twist)))
+    # ax2.fill_between(expCM_array_list[i][:, 1],
+    #                   expCM_array_list[i][:, 0]*(1 + prcnt_error),
+    #                   expCM_array_list[i][:, 0]*(1 - prcnt_error), alpha=0.25,
+    #                   color=PALETTE[i])
+    ax2.set_ylim(top=0, bottom=-0.10)
 
-    # fig, ax1 = plt.subplots(1, sharex=True, dpi=DPI)
-    # ax1.plot(CL_array, CM_array, label="VSPAERO")
-    # # ax1.plot(expCL4CM_array, expCM_array, linestyle="None",
-    # #          label="Experimental", color=PALETTE[1], marker=MARKERS[1])
-    # # ax1.fill_between(expalpha_array, expCL_array*(1 + prcnt_error),
-    # #                  expCL_array*(1 - prcnt_error), alpha=0.25,
-    # #                  color=PALETTE[1])
-    # #ax1.set_xlim(left=-0.4, right=1.0)
-    # #ax1.set_ylim(bottom=-0.15)
-
-    # ax1.set_xlabel(r"$\mathdefault{C_{L}}$")
-    # ax1.set_ylabel(r"$\mathdefault{C_{M}}$")
-    # ax1.legend()
-    # fig.savefig(os.path.join(GRAPHICS_DIR, "moment_curves.pdf"), format="pdf",
-    #             bbox_inches="tight")
+    ax2.set_xlabel(r"$\mathdefault{C_{L}}$")
+    ax2.set_ylabel(r"$\mathdefault{C_{M}}$")
+    ax2.legend()
+    fig2.savefig(os.path.join(GRAPHICS_DIR, "moment_curves.pdf"), format="pdf",
+                 bbox_inches="tight")
--- validation_cs_10/validation_cs_10-lift_dist.py
+++ validation_cs_10/validation_cs_10-lift_dist.py
@@ -1,4 +1,4 @@
-"""Run VSPAERO validation test 9."""
+"""Run VSPAERO validation test 2."""
 import openvsp as vsp
 import os
 import subprocess
@@ -11,8 +11,6 @@ DPI = 300
 PALETTE = ["darkblue", "darkorange", "darkgreen", "firebrick",
            "purple", "mediumvioletred", "goldenrod", "darkcyan"]
 MARKERS = ["o", "^", "s", "P", "d"]
-LEGEND_FONTSIZE = "small"
-
 GRAPHICS_DIR = "graphics"
 sns.set_theme(style="whitegrid", font="Palatino Linotype",
               context="paper", palette=PALETTE)
@@ -21,7 +19,7 @@ sns.set_theme(style="whitegrid", font="Palatino Linotype",
 
 OUTPUTDIR = "output"
 VSPSCRIPT = "runVSPAEROSweep.vspscript"
-FNAME = "validation_cs_9.vsp3"
+FNAME = "validation_cs_10.vsp3"
 FDIR = os.path.join(OUTPUTDIR, FNAME)
 VSPAERODIR = os.path.join(OUTPUTDIR, VSPSCRIPT)
 
@@ -29,15 +27,25 @@ VSPAERODIR = os.path.join(OUTPUTDIR, VSPSCRIPT)
 # %% User input
 
 # Wing geometry
-midspan = 63.630/12
-root_chord = 21.941/12
-tip_chord = 9.873/12
+midspan = 180/2/12
+root_chord = 28.57/12
+tip_chord = 11.43/12
 sweep_loc = 0.25
-sweep = 45
+sweep = 0
+dihedral = 3
+tipmatchdhdrl_flag = True
+twist_array = np.array([0.0, -2.0])
 
-xseccurve_type = 8
-tc_rat = 0.12
-series = 5
+tap_rat = tip_chord/root_chord
+chord_mgc = 2/3 * root_chord * (1 + tap_rat + tap_rat**2)/(1 + tap_rat)
+yloc_mgc = midspan/3 * (1 + 2*tap_rat)/(1 + tap_rat)
+xloc_mgc = root_chord/4
+zloc_mgc = 0.025
+
+xseccurve_type = vsp.XS_SIX_SERIES
+tc_rat = 0.10
+series = 2
+cl_i = 0.2
 
 # Mesh parameters
 chordwise_tess = 33
@@ -48,60 +56,59 @@ le_clstr = 0.25
 te_clstr = 0.25
 
 # Flow conditions
-alpha_i = 4.7
-alpha_f = 5
-alpha_npts = 1
-alpha_array = np.linspace(alpha_i, alpha_f, alpha_npts)
+
+alpha_max_list = [13.44, 13.42]
+
+
+mach = 0.17
 
 # Known results
 prcnt_error = 0.05
 
-expyloc_array = np.array(
-    [0.001, 0.03, 0.101, 0.301, 0.547, 0.748, 0.899, 0.96])
-excldist_array = np.array(
-    [1.057, 1.073, 1.156, 1.149, 1.067, 0.908, 0.765, 0.584])
+yloc_wsh0 = [-0.00101010101010101, 0.050505050505050504, 0.1,
+             0.1494949494949495, 0.198989898989899, 0.2494949494949495, 0.3,
+             0.34949494949494947, 0.398989898989899, 0.4505050505050505,
+             0.498989898989899, 0.5484848484848485, 0.6, 0.6494949494949495,
+             0.6989898989898989, 0.7494949494949495, 0.798989898989899,
+             0.8494949494949495, 0.9, 0.9494949494949495, 0.9282828282828283,
+             0.9666666666666667, 0.9777777777777777, 0.9878787878787879]
+cL_wsh0 = [1.1524752475247524, 1.1702970297029702, 1.194059405940594,
+           1.2099009900990099, 1.2198019801980198, 1.2356435643564356,
+           1.2435643564356436, 1.2554455445544555, 1.2613861386138614,
+           1.2673267326732673, 1.2732673267326733, 1.2752475247524753,
+           1.2752475247524753, 1.2732673267326733, 1.2673267326732673,
+           1.2594059405940594, 1.2415841584158416, 1.205940594059406,
+           1.1405940594059405, 0.9722772277227723, 1.0673267326732674,
+           0.8574257425742575, 0.7683168316831683, 0.6534653465346536]
+cLdist_wsh0 = np.array([yloc_wsh0, cL_wsh0]).T
 
 
-# %% Create OpenVSP file
+yloc_wsh2 = [-0.002167182662538708, 0.04891640866873065, 0.09814241486068112,
+             0.14922600619195048, 0.20030959752321983, 0.25046439628482975,
+             0.29876160990712075, 0.34984520123839014, 0.39907120743034064,
+             0.4510835913312694, 0.5003095975232199, 0.5513931888544893,
+             0.5996904024767803, 0.6507739938080496, 0.6981424148606812,
+             0.7520123839009288, 0.8021671826625388, 0.8532507739938081,
+             0.9034055727554181, 0.9386996904024769, 0.9591331269349846,
+             0.9749226006191952, 0.9832817337461301, 0.9916408668730652]
+cL_wsh2 = [1.1906542056074767, 1.2130841121495326, 1.2355140186915887,
+           1.250467289719626, 1.263551401869159, 1.2710280373831775,
+           1.2803738317757007, 1.2822429906542054, 1.2897196261682242,
+           1.2897196261682242, 1.2859813084112148, 1.2859813084112148,
+           1.2803738317757007, 1.2766355140186916, 1.2654205607476636,
+           1.250467289719626, 1.2205607476635514, 1.1813084112149532,
+           1.0990654205607477, 0.9925233644859813, 0.8934579439252337,
+           0.788785046728972, 0.6953271028037384, 0.6018691588785048]
+cLdist_wsh2 = np.array([yloc_wsh2, cL_wsh2]).T
 
-vsp.ClearVSPModel()
-
-# Add wing
-wing_id = vsp.AddGeom("WING")
-
-# Modify wing
-vsp.SetParmValUpdate(wing_id, "Span", "XSec_1", midspan)
-vsp.SetParmValUpdate(wing_id, "Root_Chord", "XSec_1", root_chord)
-vsp.SetParmValUpdate(wing_id, "Tip_Chord", "XSec_1", tip_chord)
-vsp.SetParmValUpdate(wing_id, "Sweep_Location", "XSec_1", sweep_loc)
-vsp.SetParmValUpdate(wing_id, "Sweep", "XSec_1", sweep)
-
-xsec_surf = vsp.GetXSecSurf(wing_id, 0)
-vsp.ChangeXSecShape(xsec_surf, 0, vsp.XS_SIX_SERIES)
-vsp.ChangeXSecShape(xsec_surf, 1, vsp.XS_SIX_SERIES)
-vsp.Update()
-vsp.SetParmValUpdate(wing_id, "Series", "XSecCurve_0", series)
-vsp.SetParmValUpdate(wing_id, "Series", "XSecCurve_1", series)
-vsp.SetParmValUpdate(wing_id, "ThickChord", "XSecCurve_0", tc_rat)
-vsp.SetParmValUpdate(wing_id, "ThickChord", "XSecCurve_1", tc_rat)
-
-# Modifiy mesh
-vsp.SetParmValUpdate(wing_id, "Tess_W", "Shape", chordwise_tess)
-vsp.SetParmValUpdate(wing_id, "SectTess_U", "XSec_1", spanwise_tess)
-vsp.SetParmValUpdate(wing_id, "InCluster", "XSec_1", root_clstr)
-vsp.SetParmValUpdate(wing_id, "OutCluster", "XSec_1", tip_clstr)
-vsp.SetParmValUpdate(wing_id, "LECluster", "WingGeom", le_clstr)
-vsp.SetParmValUpdate(wing_id, "TECluster", "WingGeom", te_clstr)
-vsp.Update()
-
-vsp.WriteVSPFile(FDIR, vsp.SET_ALL)
-vsp.Update()
+cLdist_array_list = [cLdist_wsh0, cLdist_wsh2]
 
 
 # %%Define funxtion to set analysis parameters
 
-
-def setVSPAEROsweepinput(alpha_i, alpha_f, alpha_npts, mach=0.0, x_cg=0.0, y_cg=0.0, z_cg=0.0):
+def setVSPAEROsweepinput(alpha_i, alpha_f, alpha_npts, mach=0.0, x_cg=0.0,
+                         y_cg=0.0, z_cg=0.0,
+                         chord_mgc=(root_chord+tip_chord)/2):
     with open(VSPAERODIR) as file_object:
         vspscriptlines = file_object.readlines()
     vspscriptlines[10] = '	  string fname = "{0}";\n'.format(FNAME)
@@ -110,60 +117,96 @@ def setVSPAEROsweepinput(alpha_i, alpha_f, alpha_npts, mach=0.0, x_cg=0.0, y_cg=
     vspscriptlines[57] = "    double alpha_npts = {0};\n".format(alpha_npts)
     vspscriptlines[62] = "    double mach_i = {0:.2f};\n".format(mach)
 
-    vspscriptlines[68] = "    double x_cg = {0:.2f};\n".format(x_cg)
-    vspscriptlines[73] = "    double y_cg = {0:.2f};\n".format(y_cg)
-    vspscriptlines[78] = "    double z_cg = {0:.2f};\n".format(z_cg)
+    vspscriptlines[68] = "    double x_cg = {0:.3f};\n".format(x_cg)
+    vspscriptlines[73] = "    double y_cg = {0:.3f};\n".format(y_cg)
+    vspscriptlines[78] = "    double z_cg = {0:.3f};\n".format(z_cg)
+
+    vspscriptlines[84] = "    double chord_mgc = {0:.3f};\n".format(chord_mgc)
 
     with open(VSPAERODIR, "w") as file_object:
         file_object.writelines(vspscriptlines)
 
 
-# %% Chordwise tesselation sensitivity analysis
+# %% Create OpenVSP file
 
-setVSPAEROsweepinput(alpha_i, alpha_f, alpha_npts)
+for i, twist in enumerate(twist_array):
+    alpha_i = alpha_max_list[i]
+    alpha_f = alpha_i + 1
+    alpha_npts = 1
+    alpha_array = np.linspace(alpha_i, alpha_f, alpha_npts)
 
-vsp.ClearVSPModel()
-vsp.ReadVSPFile(FDIR)
-vsp.Update()
+    vsp.ClearVSPModel()
 
-subprocess.run(["vsp", "-script", VSPSCRIPT], shell=True, cwd=OUTPUTDIR)
+    # Add wing
+    wing_id = vsp.AddGeom("WING")
 
+    # Modify wing
+    vsp.SetParmValUpdate(wing_id, "Span", "XSec_1", midspan)
+    vsp.SetParmValUpdate(wing_id, "Root_Chord", "XSec_1", root_chord)
+    vsp.SetParmValUpdate(wing_id, "Tip_Chord", "XSec_1", tip_chord)
+    vsp.SetParmValUpdate(wing_id, "Sweep_Location", "XSec_1", sweep_loc)
+    vsp.SetParmValUpdate(wing_id, "Sweep", "XSec_1", sweep)
+    vsp.SetParmValUpdate(wing_id, "Dihedral", "XSec_1", dihedral)
+    vsp.SetParmValUpdate(wing_id, "Twist", "XSec_1", twist)
+    vsp.SetParmValUpdate(wing_id, "RotateMatchDideralFlag",
+                         "XSec_1", tipmatchdhdrl_flag)
 
-# %% Extract load distribution data
+    xsec_surf = vsp.GetXSecSurf(wing_id, 0)
+    vsp.ChangeXSecShape(xsec_surf, 0, xseccurve_type)
+    vsp.ChangeXSecShape(xsec_surf, 1, xseccurve_type)
+    vsp.Update()
+    vsp.SetParmValUpdate(wing_id, "Series", "XSecCurve_0", series)
+    vsp.SetParmValUpdate(wing_id, "Series", "XSecCurve_1", series)
+    vsp.SetParmValUpdate(wing_id, "ThickChord", "XSecCurve_0", tc_rat)
+    vsp.SetParmValUpdate(wing_id, "ThickChord", "XSecCurve_1", tc_rat)
+    vsp.SetParmValUpdate(wing_id, "IdealCl", "XSecCurve_0", cl_i)
+    vsp.SetParmValUpdate(wing_id, "IdealCl", "XSecCurve_1", cl_i)
 
-loaddist_array = np.loadtxt(
-    os.path.join(OUTPUTDIR, FNAME[:-5] + "_DegenGeom.lod"), skiprows=19,
-    max_rows=spanwise_tess-1)
+    # Modifiy mesh
+    vsp.SetParmValUpdate(wing_id, "Tess_W", "Shape", chordwise_tess)
+    vsp.SetParmValUpdate(wing_id, "SectTess_U", "XSec_1", spanwise_tess)
+    vsp.SetParmValUpdate(wing_id, "InCluster", "XSec_1", root_clstr)
+    vsp.SetParmValUpdate(wing_id, "OutCluster", "XSec_1", tip_clstr)
+    vsp.SetParmValUpdate(wing_id, "LECluster", "WingGeom", le_clstr)
+    vsp.SetParmValUpdate(wing_id, "TECluster", "WingGeom", te_clstr)
+    vsp.Update()
 
-refgeom_array = np.loadtxt(
-    os.path.join(OUTPUTDIR, FNAME[:-5] + "_DegenGeom.lod"), skiprows=3,
-    max_rows=14, usecols=1)
+    vsp.WriteVSPFile(FDIR, vsp.SET_ALL)
+    vsp.Update()
 
-polar_array = np.loadtxt(
-    os.path.join(OUTPUTDIR, FNAME[:-5] + "_DegenGeom.polar"), skiprows=1)
+    setVSPAEROsweepinput(alpha_i, alpha_f, alpha_npts,
+                         mach=mach, x_cg=xloc_mgc, z_cg=zloc_mgc,
+                         chord_mgc=chord_mgc)
 
-cldist_vsp = loaddist_array[:, 7]
-yloc_array = loaddist_array[:, 1]
-chord_array = loaddist_array[:, 5]
-cref = refgeom_array[1]
-CL = polar_array[4]
-newcldist = cldist_vsp*chord_array/(cref*CL)
+    vsp.ClearVSPModel()
+    vsp.ReadVSPFile(FDIR)
+    vsp.Update()
 
+    subprocess.run(["vsp", "-script", VSPSCRIPT], shell=True, cwd=OUTPUTDIR)
 
-# %% Plot results
+    loaddist_array = np.loadtxt(
+        os.path.join(OUTPUTDIR, FNAME[:-5] + "_DegenGeom.lod"), skiprows=19,
+        max_rows=spanwise_tess-1)
 
-fig, ax1 = plt.subplots(1, sharex=True, dpi=DPI)
-ax1.plot(yloc_array, newcldist, label="VSPAERO")
-ax1.plot(expyloc_array, excldist_array, linestyle="None",
-         color=PALETTE[1], marker=MARKERS[1], label="Experimental")
-# ax1.fill_between(expyloc_array, excldist_array*(1 + prcnt_error),
-#                  excldist_array*(1 - prcnt_error), alpha=0.25,
-#                  color=PALETTE[1])
-ax1.set_xlim(left=0)
-ax1.set_ylim(bottom=0)
-ax1.set_xlabel(r"$\mathdefault{\dfrac{2y}{b}}$")
-ax1.set_ylabel(r"$\mathdefault{\dfrac{c_{L}\bullet c}{C_{L}\bullet c_{ref}}}$")
-ax1.legend()
+    refgeom_array = np.loadtxt(
+        os.path.join(OUTPUTDIR, FNAME[:-5] + "_DegenGeom.lod"), skiprows=3,
+        max_rows=14, usecols=1)
 
-fig.savefig(os.path.join(GRAPHICS_DIR, "lift_dist.pdf"), format="pdf",
-            bbox_inches="tight")
+    cL_array = loaddist_array[:, 7]
+    yloc_array = loaddist_array[:, 1]
+
+    fig1, ax1 = plt.subplots(1, sharey=True, dpi=DPI)
+    ax1.plot(yloc_array, cL_array, label="VSPAERO")
+    ax1.plot(cLdist_array_list[i][:, 0], cLdist_array_list[i][:, 1], linestyle="None",
+             color=PALETTE[1], marker=MARKERS[1], label="Experimental")
+    # ax1.fill_between(cLdist_array_list[i][:, 0], cLdist_array_list[i][:, 1]*(1 + prcnt_error),
+    #                   cLdist_array_list[i][:, 1]*(1 - prcnt_error), alpha=0.25,
+    #                   color=PALETTE[1])
+    ax1.set_xlim(left=0)
+    ax1.set_ylim(bottom=0)
+    ax1.set_xlabel(r"$\mathdefault{\dfrac{2y}{b}}$")
+    ax1.set_ylabel(r"$\mathdefault{c_{L}}$")
+    ax1.legend()
+
+    fig1.savefig(os.path.join(GRAPHICS_DIR, "lift_dist-wsht{0}.pdf".format(twist)), format="pdf",
+                 bbox_inches="tight")
